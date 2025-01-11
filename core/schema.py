fc63da782f8db83281d8fba0df4d44da0f628cd2
Vicente Jorquera <jorquerav07@gmail.com>
2025-01-11 17:53:11 -0300

    se integra graphene y se realiza CRUD básico

---
+++ core/schema.py
@@ -0,0 +1,72 @@
+import graphene
+from graphene_django import DjangoObjectType
+from parking.models import ParkingSpot
+
+
+class ParkingType(DjangoObjectType):
+    class Meta:
+        model = ParkingSpot
+        fields = ('sensor_id', 'sensor_status', 'sensor_location', 'sensor_type' )
+        
+class CreateSpot(graphene.Mutation):
+    class Arguments:
+        sensor_id = graphene.String()
+        sensor_status = graphene.Boolean()
+        sensor_location = graphene.String()
+        sensor_type = graphene.String()
+        
+    spot = graphene.Field(ParkingType)
+    
+    def mutate(self, info, sensor_id, sensor_status, sensor_location, sensor_type):
+        spot = ParkingSpot(sensor_id=sensor_id, sensor_status=sensor_status, sensor_location=sensor_location, sensor_type=sensor_type)
+        spot.save()
+        return CreateSpot(spot)
+
+class DeleteSpot(graphene.Mutation):
+    class Arguments:
+        sensor_id = graphene.String()
+        
+    ok = graphene.Boolean()
+    
+    def mutate(self, info, sensor_id):
+        spot = ParkingSpot.objects.get(sensor_id=sensor_id)
+        spot.delete()
+        return DeleteSpot(ok=True)
+
+
+class UpdateSpot(graphene.Mutation):
+    class Arguments:
+        sensor_id = graphene.String()
+        sensor_status = graphene.Boolean()
+        
+    spot = graphene.Field(ParkingType)
+    
+    def mutate(self, info, sensor_id, sensor_status):
+        spot = ParkingSpot.objects.get(sensor_id=sensor_id)
+        spot.sensor_status = sensor_status
+        spot.save()
+        return UpdateSpot(spot)
+
+# MÉTODOS GET O EQUIVALENTE
+class Query(graphene.ObjectType):
+    hello = graphene.String(name=graphene.String(default_value="stranger"))
+    spots = graphene.List(ParkingType)
+    spot = graphene.Field(ParkingType, sensor_id=graphene.ID())
+
+    def resolve_hello(self, info, name):
+        return f"Hello {name}"
+      
+    def resolve_spots(self, info):
+        return ParkingSpot.objects.all()
+      
+    def resolve_spot(self, info, sensor_id):
+        return ParkingSpot.objects.get(sensor_id=sensor_id)
+      
+# MUTACIONES (POST, PUT, DELETE) O EQUIVALENTE
+class Mutation(graphene.ObjectType):
+  create_spot = CreateSpot.Field()
+  delete_spot = DeleteSpot.Field()
+  update_spot = UpdateSpot.Field()
+  
+      
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'parking'
+    'parking',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
